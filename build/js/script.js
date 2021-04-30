8ce94d5ba091778be80af4489caa7cddab3ba646
Кирилл <kkirillcore@gmail.com>
2021-04-30 11:37:36 +0300

    вносит правки в стилизацию и добавляет валидацию

--- build/js/script.js
+++ build/js/script.js
@@ -1,6 +1,40 @@
 "use strict";
 
 var questionsBlock = document.querySelector(".la-questions");
+var header = document.querySelector(".la-header");
+var requestForm = document.querySelector(".la-request__form");
+var companyName = requestForm.querySelector(".la-request__item--company");
+var companyPersonName = requestForm.querySelector(".la-request__item--contact");
+var companyPhone = requestForm.querySelector(".la-request__item--phone");
+var companyEmail = requestForm.querySelector(".la-request__item--email");
+var formItemBlocks = requestForm.querySelectorAll(".la-request__item");
+var ESC_KEYCODE = 27;
+var blackout = document.querySelector(".la-blackout");
+var bodyGlobal = document.querySelector("body");
+var thanksModal = document.querySelector(".la-thanks");
+var closeThanksButton = thanksModal.querySelector(".js-close-thanks-modal");
+
+var showBlackout = function showBlackout() {
+  if (blackout.classList.contains("la-blackout--hidden")) {
+    blackout.classList.remove("la-blackout--hidden");
+  }
+};
+
+var hideBlackout = function hideBlackout() {
+  if (!blackout.classList.contains("la-blackout--hidden")) {
+    blackout.classList.add("la-blackout--hidden");
+  }
+};
+
+var blockBackground = function blockBackground() {
+  bodyGlobal.style.overflow = "hidden";
+  bodyGlobal.style.touchAction = "none";
+};
+
+var unBlockBackground = function unBlockBackground() {
+  bodyGlobal.style.overflow = "";
+  bodyGlobal.style.touchAction = "";
+};
 
 if (questionsBlock) {
   var questionsButtons = questionsBlock.querySelectorAll(".js-questions-button");
@@ -19,7 +53,6 @@ if (questionsBlock) {
   });
 }
 
-var header = document.querySelector(".la-header");
 window.addEventListener("scroll", function () {
   var requestLinkTop = document.querySelector(".js-request-link").getBoundingClientRect().top;
 
@@ -29,3 +62,92 @@ window.addEventListener("scroll", function () {
     header.classList.remove("la-header--fixed");
   }
 });
+
+var resetErrorOnInput = function resetErrorOnInput(item, errorClass) {
+  var input = item.querySelector("input");
+
+  if (input.classList.contains(errorClass)) {
+    input.classList.remove(errorClass);
+  }
+};
+
+var validateText = function validateText(textItem, errorClass) {
+  var name = textItem.querySelector("input").value;
+
+  if (!name) {
+    textItem.classList.add(errorClass);
+    return false;
+  } else {
+    resetErrorOnInput(textItem, errorClass);
+    return true;
+  }
+};
+
+var validatePhone = function validatePhone(phoneItem, errorClass) {
+  var phone = phoneItem.querySelector("input").value;
+  var regEx = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+
+  if (regEx.test(phone) === false) {
+    phoneItem.classList.add(errorClass);
+    return false;
+  } else {
+    resetErrorOnInput(phoneItem, errorClass);
+    return true;
+  }
+};
+
+var validateEmail = function validateEmail(emailItem, errorClass) {
+  var mail = emailItem.querySelector("input").value;
+  var regEx = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+  if (regEx.test(mail) === false) {
+    emailItem.classList.add(errorClass);
+    return false;
+  } else {
+    resetErrorOnInput(emailItem, errorClass);
+    return true;
+  }
+};
+
+var removeClassWithChecking = function removeClassWithChecking(item, className) {
+  if (item.classList.contains(className)) {
+    item.classList.remove(className);
+  }
+};
+
+var resetFormErrors = function resetFormErrors() {
+  formItemBlocks.forEach(function (item) {
+    removeClassWithChecking(item, "la-request__item--error");
+  });
+};
+
+var closeThanksModal = function closeThanksModal() {
+  if (!thanksModal.classList.contains("la-thanks--hidden")) {
+    thanksModal.classList.add("la-thanks--hidden");
+    unBlockBackground();
+    hideBlackout();
+    blackout.removeEventListener("click", closeThanksModal);
+    document.removeEventListener("keydown", closeThanksModalWithEsc);
+    requestForm.reset();
+  }
+};
+
+var closeThanksModalWithEsc = function closeThanksModalWithEsc(evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closeThanksModal();
+  }
+};
+
+requestForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  resetFormErrors();
+
+  if (validateEmail(companyEmail, "la-request__item--error") && validateText(companyName, "la-request__item--error") && validatePhone(companyPhone, "la-request__item--error") && validateText(companyPersonName, "la-request__item--error")) {
+    blockBackground();
+    showBlackout();
+    removeClassWithChecking(thanksModal, "la-thanks--hidden");
+    blackout.addEventListener("click", closeThanksModal);
+    document.addEventListener("keydown", closeThanksModalWithEsc);
+  }
+});
+closeThanksButton.addEventListener("click", closeThanksModal);
--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,43 @@
 const questionsBlock = document.querySelector(`.la-questions`);
+const header = document.querySelector(`.la-header`);
+
+const requestForm = document.querySelector(`.la-request__form`);
+const companyName = requestForm.querySelector(`.la-request__item--company`);
+const companyPersonName = requestForm.querySelector(`.la-request__item--contact`);
+const companyPhone = requestForm.querySelector(`.la-request__item--phone`);
+const companyEmail = requestForm.querySelector(`.la-request__item--email`);
+
+const formItemBlocks = requestForm.querySelectorAll(`.la-request__item`);
+
+const ESC_KEYCODE = 27;
+const blackout = document.querySelector(`.la-blackout`);
+const bodyGlobal = document.querySelector(`body`);
+
+const thanksModal = document.querySelector(`.la-thanks`);
+const closeThanksButton = thanksModal.querySelector(`.js-close-thanks-modal`);
+
+const showBlackout = () => {
+  if (blackout.classList.contains(`la-blackout--hidden`)) {
+    blackout.classList.remove(`la-blackout--hidden`);
+  }
+};
+
+const hideBlackout = () => {
+  if (!blackout.classList.contains(`la-blackout--hidden`)) {
+    blackout.classList.add(`la-blackout--hidden`);
+  }
+};
+
+const blockBackground = () => {
+  bodyGlobal.style.overflow = `hidden`;
+  bodyGlobal.style.touchAction = `none`;
+};
+
+const unBlockBackground = () => {
+  bodyGlobal.style.overflow = ``;
+  bodyGlobal.style.touchAction = ``;
+};
+
 
 if (questionsBlock) {
   const questionsButtons = questionsBlock.querySelectorAll(`.js-questions-button`);
@@ -18,8 +57,6 @@ if (questionsBlock) {
   });
 }
 
-const header = document.querySelector(`.la-header`);
-
 window.addEventListener(`scroll`, () => {
   let requestLinkTop = document.querySelector(`.js-request-link`).getBoundingClientRect().top;
 
@@ -29,3 +66,105 @@ window.addEventListener(`scroll`, () => {
     header.classList.remove(`la-header--fixed`);
   }
 });
+
+const resetErrorOnInput = (item, errorClass) => {
+  const input = item.querySelector(`input`);
+
+  if (input.classList.contains(errorClass)) {
+    input.classList.remove(errorClass);
+  }
+};
+
+
+const validateText = (textItem, errorClass) => {
+  const name = textItem.querySelector(`input`).value;
+  if (!name) {
+    textItem.classList.add(errorClass);
+
+    return false;
+  } else {
+    resetErrorOnInput(textItem, errorClass);
+
+    return true;
+  }
+};
+
+const validatePhone = (phoneItem, errorClass) => {
+  const phone = phoneItem.querySelector(`input`).value;
+  const regEx = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+
+  if (regEx.test(phone) === false) {
+    phoneItem.classList.add(errorClass);
+
+    return false;
+  } else {
+
+    resetErrorOnInput(phoneItem, errorClass);
+
+    return true;
+  }
+};
+
+const validateEmail = (emailItem, errorClass) => {
+  const mail = emailItem.querySelector(`input`).value;
+  const regEx = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+
+  if (regEx.test(mail) === false) {
+    emailItem.classList.add(errorClass);
+
+    return false;
+  } else {
+    resetErrorOnInput(emailItem, errorClass);
+
+    return true;
+  }
+};
+
+const removeClassWithChecking = (item, className) => {
+  if (item.classList.contains(className)) {
+    item.classList.remove(className);
+  }
+};
+
+const resetFormErrors = () => {
+  formItemBlocks.forEach((item) => {
+    removeClassWithChecking(item, `la-request__item--error`);
+  });
+};
+
+const closeThanksModal = () => {
+  if (!thanksModal.classList.contains(`la-thanks--hidden`)) {
+    thanksModal.classList.add(`la-thanks--hidden`);
+
+    unBlockBackground();
+    hideBlackout();
+
+    blackout.removeEventListener(`click`, closeThanksModal);
+    document.removeEventListener(`keydown`, closeThanksModalWithEsc);
+
+    requestForm.reset();
+  }
+};
+
+const closeThanksModalWithEsc = (evt) => {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closeThanksModal();
+  }
+};
+
+
+requestForm.addEventListener(`submit`, (evt) => {
+  evt.preventDefault();
+  resetFormErrors();
+
+  if (validateEmail(companyEmail, `la-request__item--error`) && validateText(companyName, `la-request__item--error`) && validatePhone(companyPhone, `la-request__item--error`) && validateText(companyPersonName, `la-request__item--error`)) {
+    blockBackground();
+    showBlackout();
+
+    removeClassWithChecking(thanksModal, `la-thanks--hidden`);
+    blackout.addEventListener(`click`, closeThanksModal);
+    document.addEventListener(`keydown`, closeThanksModalWithEsc);
+  }
+});
+
+closeThanksButton.addEventListener(`click`, closeThanksModal);
